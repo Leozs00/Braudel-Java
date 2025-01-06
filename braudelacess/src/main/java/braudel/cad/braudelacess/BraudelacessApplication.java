a8e6a32e960f37b3d3645bfcbc0139994f3806be
Leonardo <Leozs00@users.noreply.github.com>
2025-01-06 11:57:06 -0300

    Primeiro commit do projeto

---
+++ braudelacess/src/main/java/braudel/cad/braudelacess/BraudelacessApplication.java
@@ -0,0 +1,13 @@
+package braudel.cad.braudelacess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BraudelacessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BraudelacessApplication.class, args);
+	}
+
+}
---
+++ braudelacess/src/test/java/braudel/cad/braudelacess/BraudelacessApplicationTests.java
@@ -0,0 +1,13 @@
+package braudel.cad.braudelacess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BraudelacessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
